bdb555ae080179c32605df1643335e8171e5f508
Gracia Kanna <99242649+kannagracia@users.noreply.github.com>
2022-06-13 05:34:58 +0700

    Added new Upload Button on HomeFragment.kt

--- app/src/main/java/com/example/handairupaapp/ui/home/menulist/home/HomeFragment.kt
+++ app/src/main/java/com/example/handairupaapp/ui/home/menulist/home/HomeFragment.kt
@@ -1,13 +1,18 @@
 package com.example.handairupaapp.ui.home.menulist.home
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.handairupaapp.databinding.FragmentHomeBinding
 import com.example.handairupaapp.ui.camera.CameraActivity
+import com.example.handairupaapp.ui.result.ResultActivity
+import com.example.handairupaapp.utils.rotateBitmap
+import java.io.File
 
 class HomeFragment : Fragment() {
 
@@ -27,16 +32,43 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
-            cameraXButton.setOnClickListener {
-                Intent(requireContext(), CameraActivity::class.java).also {
-                    startActivity(it)
-                }
+            cameraXButton.setOnClickListener { startCameraX() }
+            uploadButton.setOnClickListener { startUpload() }
             }
         }
+
+    private fun startCameraX() {
+        val intent = Intent(activity, CameraActivity::class.java)
+            launcherIntentCameraX.launch(intent)
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == ResultActivity.CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+
+            val getFile = myFile
+            val result = rotateBitmap(
+                BitmapFactory.decodeFile(getFile.path),
+                isBackCamera
+            )
+            binding.ivPreviewImage.setImageBitmap(result)
+        }
+    }
+
+    private fun startUpload() {
+        val intent = Intent(activity, ResultActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
     }
 }
