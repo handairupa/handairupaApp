6e29713ddcf6892c928fe37b804f27037c79013a
Gracia Kanna <99242649+kannagracia@users.noreply.github.com>
2022-06-13 06:46:26 +0700

    Adjust ImageView on fragment_home.xml

--- app/src/main/java/com/example/handairupaapp/model/SkinConditionModel.kt
+++ app/src/main/java/com/example/handairupaapp/model/SkinConditionModel.kt
@@ -8,9 +8,4 @@ import java.io.File
 data class SkinConditionModel (
     val skin_condition_name: String,
     val skin_condition_image: Int
-/*    val user_email: String,
-    val skin_condition_id: Int,
-
-    val skin_condition_probability: String,
-    val skin_condition_notes: String,*/
 ) : Parcelable
